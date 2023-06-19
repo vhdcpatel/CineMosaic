5aa778e304fd4972f9cde50216c988a7bc14b2f9
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-19 18:00:08 +0530

    added empty pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 import { useEffect, useState } from 'react';
 import ApiCall from './utils/apiCall';
-import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { getApiConfiguration } from './store/Slices/homeSlice';
+import './App.css';
+import Footer from './components/footer/Footer';
 
 function App() {
-  const url = "/movie/now_playing"; 
-  
-  useEffect(()=>{
-    apiTesting();
+  const [data2,setData] = useState('')
+
+  const Callurl = "/movie/now_playing"; 
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.home.url)
+  console.log(data);
+
+  useEffect(() => {
+     apiTesting();
   },[]);
 
   const apiTesting = async () => {
-    const res = await ApiCall(url);
-    console.log(res);   
+    const res = await ApiCall(Callurl);
+    setData(res);
+    dispatch(getApiConfiguration(res));
     }
 
   return (
-    <main className='mainBackGround'>
-      <h1>Hello world from vhdc!</h1>
+    <main className="mainBackGround">
+      {/* <h1>Hello world from vhdc!</h1> */}
+      {/* {data?.total_results} */}
+      <Footer></Footer>
     </main>
-  )
+  );
 }
 
 export default App
--- src/store/Slices/homeSlice.jsx
+++ src/store/Slices/homeSlice.jsx
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// setting the intial state.
 const initialState = {
   url: {},
   genres: {},
