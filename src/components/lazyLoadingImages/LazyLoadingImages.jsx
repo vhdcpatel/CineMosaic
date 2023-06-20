a2975e037f7083328355b93f6f52e73b853255b8
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-20 14:00:48 +0530

    loaded and retrive img  config from store

---
+++ src/components/lazyLoadingImages/LazyLoadingImages.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+const LazyLoadingImages = ({src,className}) => {
+  return (
+  <div>
+    <LazyLoadImage>
+
+    </LazyLoadImage>
+  </div>
+  )
+}
+
+export default LazyLoadingImages
