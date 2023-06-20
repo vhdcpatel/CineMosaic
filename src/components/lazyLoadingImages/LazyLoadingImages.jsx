50e0c677bc50d1abb4a6f0f30f32014bcab7686a
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-20 17:44:29 +0530

    added lazyloading for images.

--- src/components/lazyLoadingImages/LazyLoadingImages.jsx
+++ src/components/lazyLoadingImages/LazyLoadingImages.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const LazyLoadingImages = ({src,className}) => {
   return (
   <div>
-    <LazyLoadImage>
-
+    <LazyLoadImage
+    className={className || ""}
+    alt=""
+    effect='blur'
+    src={src}
+    >
     </LazyLoadImage>
   </div>
   )
