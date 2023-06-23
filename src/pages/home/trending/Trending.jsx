81e6892085a2d1a9eb475244f505172f7fe098e8
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-23 14:44:02 +0530

    added API call and pass data as props in  carousel

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Wrapper from '../../../components/wrapper/Wrapper'
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
+import useFetch from '../../../hooks/useFetch';
 import "./Trending.css";
+import Carousel from '../../../components/carousel/Carousel';
 
 const Trending = () => {
 
-  const onTabChange = (tab)=>{
+  const endPoints = ["day","week"];
+  const [endPoint,setEndPoint] = useState(endPoints[0])
+  const path = `/trending/all/${endPoint}`;
+  
+  const {data, loading} = useFetch(path);
 
+  console.log(data);
+
+  const onTabChange = (tab)=>{
+    setEndPoint(tab)
   }
 
   return (
     <div className="carouselSection">
       <Wrapper>
         <span className="carouselTitle">Trending</span>
-        <SwitchTabs data={["day", "Week"]} onTabChange={onTabChange} />
+        <SwitchTabs data={[...endPoints]} onTabChange={onTabChange} />
       </Wrapper>
+      <Carousel data={data?.results} isLoading={loading}></Carousel>
     </div>
   );
 }
