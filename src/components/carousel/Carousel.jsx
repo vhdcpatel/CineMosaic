f1ded4e425122fe75dc55fe7256efa35c362138c
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-23 17:16:39 +0530

    added some functionality inside

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   BsFillArrowLeftCircleFill,
   BsFillArrowRightCircleFill,
@@ -12,10 +12,39 @@ import LazyLoadingImages  from '../lazyLoadingImages/LazyLoadingImages';
 import PosterFallback from "../../assets/no-poster.png";
 
 
-const Carousel = () => {
+const Carousel = ({ data, isLoading, }) => {
+  const carouselContainer = useRef();
+  const { url } = useSelector((state) => state.home);
+  const navigate = useNavigate();
+
+  const navigation = (dir) => {};
+
   return (
-    <div>Carousel</div>
-  )
-}
+    <div className="carousel">
+      <Wrapper>
+        <BsFillArrowLeftCircleFill
+          className="arrow carouselLeftNav"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="arrow carouselRightNav"
+          onClick={() => navigation("right")}
+        />
+        {!isLoading ? (
+          <div className="carouselItems">
+            {data?.map((item,index) =>{
+              console.log(index);
+              return(
+                <div key={item.id} className="carouselItem"></div>
+              );
+            })}
+          </div>
+        ) : (
+          <span>Loading</span>
+        )}
+      </Wrapper>
+    </div>
+  );
+};
 
 export default Carousel;
