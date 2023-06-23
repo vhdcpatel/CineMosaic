b4df576786f371dd727ce4de9d870eae5fc2d1d1
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-23 14:43:10 +0530

    some random changes.

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Error = () => {
+  // console.log("inside error page");
+  const inlineStyles = {
+    color: "white",
+    minHeight: "80vh",
+    fontSize: "40px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    // Add more CSS properties as needed
+  };
+
   return (
-    <div>Error</div>
+    <div style={inlineStyles}>Page not found</div>
   )
 }
 
