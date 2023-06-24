48e49d544ed21efcce9a8da34655fc29b480c94a
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-24 23:57:34 +0530

    major update in carousel section.

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -10,18 +10,49 @@ import dayjs from 'dayjs';
 import Wrapper from '../wrapper/Wrapper';
 import LazyLoadingImages  from '../lazyLoadingImages/LazyLoadingImages';
 import PosterFallback from "../../assets/no-poster.png";
+import CircleRating from '../circleRating/circleRating';
+import Genres from '../genres/Genres';
+
+import "./Carousel.css";
 
 
-const Carousel = ({ data, isLoading, }) => {
+const Carousel = ({ data, isLoading,endpoint,title }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
 
-  const navigation = (dir) => {};
+  const navigation = (dir) => {
+    const container = carouselContainer.current;
+
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
+
+    container.scrollTo({
+      left: scrollAmount,
+      behavior:"smooth",
+    });
+    
+  };
+
+  const skItem = ()=>{
+    return (
+      <div className="skeletonItem">
+        <div className="posterBlock skeleton"></div>
+        <div className="textBlock">
+          <div className="title skeleton"></div>
+          <div className="date skeleton"></div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="carousel">
       <Wrapper>
+        {title && <div className="carouselTitle">{title}</div>}
+
         <BsFillArrowLeftCircleFill
           className="arrow carouselLeftNav"
           onClick={() => navigation("left")}
@@ -31,16 +62,37 @@ const Carousel = ({ data, isLoading, }) => {
           onClick={() => navigation("right")}
         />
         {!isLoading ? (
-          <div className="carouselItems">
-            {data?.map((item,index) =>{
-              console.log(index);
-              return(
-                <div key={item.id} className="carouselItem"></div>
+          <div className="carouselItems" ref={carouselContainer}>
+            {data?.map((item, index) => {
+              const posterUrl = item.poster_path
+                ? url.poster + item.poster_path
+                : PosterFallback;
+              return (
+                <div key={item.id} className="carouselItem" onClick={()=>{navigate(`/${item.media_type}/${item.id}`)}}>
+                  <div className="posterBlock">
+                    <LazyLoadingImages src={posterUrl} />
+                    {/* Need Update */}
+                    {/* <CircleRating  rating={item.vote_average.toFixed(1)}/> */}
+                    <Genres data={item.genre_ids.slice(0, 2)} />
+                  </div>
+                  <div className="textBlock">
+                    <span className="title">{item.title || item.name}</span>
+                    <span className="date">
+                      {dayjs(item.release_Date).format("MMM D, YYYY")}
+                    </span>
+                  </div>
+                </div>
               );
             })}
           </div>
         ) : (
-          <span>Loading</span>
+          <div className="loadingSkeleton">
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+          </div>
         )}
       </Wrapper>
     </div>
