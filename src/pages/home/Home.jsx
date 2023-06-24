c9a4ccfab41612af5428d86f35d5e0c0c7f64add
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-25 00:37:21 +0530

    added Popular and Top Rated section.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import HeroBanner from './heroBanner/HeroBanner'
 import Trending from './trending/Trending'
+import Popular from './popular/Popular'
+import TopRated from './topRated/TopRated'
 
 import "./Home.css"
 
@@ -9,7 +11,8 @@ const Home = () => {
     <div className='homePage'>
       <HeroBanner/>
       <Trending/>
-      <div style={{height:1000}}></div>
+      <Popular/>
+      <TopRated/>
     </div>
   )
 }
