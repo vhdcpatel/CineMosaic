e34998f2538c72ba8df0fc091eee7d0056d957df
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-25 11:22:25 +0530

    initial changes.

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import useFetch from '../../hooks/useFetch'
+
 
 const Details = () => {
+  const { mediaType, id } = useParams();
+  const {data, loading} = useFetch(``) 
+  
   return (
     <div>Details</div>
   )
