d5281141fed4228f1c36c76c994c632fe25b7cf7
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-25 22:57:00 +0530

    added Banner section and load video and crew data

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import DetailsBanner from './detailsBanner/DetailsBanner'
 
 import useFetch from '../../hooks/useFetch'
 
 
 const Details = () => {
   const { mediaType, id } = useParams();
-  const {data, loading} = useFetch(``) 
-  
+  const { data: videoData, loading: videoDataLoading } = useFetch(
+    `/${mediaType}/${id}/videos`
+  );
+  const { data: creditsData, loading: creditsDataLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
+
+
+
   return (
-    <div>Details</div>
-  )
+    <DetailsBanner video={videoData?.results[0]} crew={creditsData?.crew} />
+  );
 }
 
 export default Details
