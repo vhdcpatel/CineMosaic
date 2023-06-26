ff3377b404728b4584e1e53e904e6a55f108b3e8
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-26 18:55:14 +0530

    Update name of circle Rating.

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import LazyLoadingImages from "../lazyLoadingImages/LazyLoadingImages";
-import CircleRating from "../circleRating/circleRating";
+import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
 import PosterFallback from "../../assets/no-poster.png";
 
