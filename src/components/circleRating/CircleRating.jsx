dd306825d258d5dd0f89650fa04092def1df0c90
vhdcpatel <vrajhpatel21@gmail.com>
2023-06-26 19:05:24 +0530

    updated name.

--- src/components/circleRating/CircleRating.jsx
+++ src/components/circleRating/CircleRating.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
-import "./CircleRating.css";
+import "./styles.css";
 
 const CircleRating = ({ rating }) => {
   return (
