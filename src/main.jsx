aa5c1d0afd16b3bbe207c8dc8bed600d49e110c3
vhdcpatel <vrajhpatel21@gmail.com>
2024-01-16 22:22:14 +0530

    Added SpeedInsights for performance measurement

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { store } from './store/store.jsx'
 import { Provider } from 'react-redux'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <App />
+        <SpeedInsights />
     </Provider>
 )
