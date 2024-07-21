d287c2288f2928ef273f8de58f6510473bbebbed
vhdcpatel <vrajhpatel21@gmail.com>
2024-07-21 12:02:53 +0530

    Added Dialog box for Network Error. - Added dialog box for network_timeout and show dialog box. - In Dialog box gives user info for using the VPN.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getApiConfiguration, getGenres } from "./store/Slices/homeSlice";
 import {  BrowserRouter as Router } from "react-router-dom";
@@ -6,9 +6,16 @@ import './App.css';
 
 import ApiCall from './utils/apiCall';
 import AppRoutes from './routes/AppRoutes';
+import VPNDialog from './components/vpnDialog/VPNDialog';
+import axios from 'axios';
 
 function App() {
   const dispatch = useDispatch();
+  const [vpnDialogBoxVisible, setVpnDialogBoxVisible] = useState(false);
+
+  const handleDialogClose = ()=>{
+    setVpnDialogBoxVisible(false);
+  }
 
   useEffect(() => {
     fetchApiConfiguration();
@@ -17,42 +24,56 @@ function App() {
 
   const fetchApiConfiguration = async () => {
     try {
-      const responce = await ApiCall("/configuration")
+      const response = await ApiCall("/configuration");
+      if(axios.isAxiosError(response)){
+        throw response;
+      }
+
       const url = {
-        backdrop: responce.images.secure_base_url + "original",
-        poster: responce.images.secure_base_url + "original",
-        profile: responce.images.secure_base_url + "original",
+        backdrop: response.images.secure_base_url + "original",
+        poster: response.images.secure_base_url + "original",
+        profile: response.images.secure_base_url + "original",
       };
       dispatch(getApiConfiguration(url))
     }
     catch (error) {
       console.log(error);
-      console.log("Someting went wrong !");
+      if(error.code === 'ERR_NETWORK'){
+        setVpnDialogBoxVisible(true);
+      }
     }
   }
 
   const genresCall = async () => {
-    let promises = [];
-    let endPoints = ["tv", "movie"];
-    let allGenres = {};
+    // Wait for response for both of api call;
+    try{
+      let promises = [];
+      let endPoints = ["tv", "movie"];
+      let allGenres = {};
+  
+      endPoints.forEach((url) => {
+        promises.push(ApiCall(`/genre/${url}/list`));
+      });
+      const data = await Promise.all(promises);
+      
+      if(axios.isAxiosError(data)){
+        throw response;
+      }
+  
+      data.map(({ genres }) => {
+        genres.map((item) => (allGenres[item.id] = item.name));
+      });
+  
+      dispatch(getGenres(allGenres));
 
-    endPoints.forEach((url) => {
-      promises.push(ApiCall(`/genre/${url}/list`));
-    });
-
-    // Wait for responce for both of api call;
-    const data = await Promise.all(promises);
-    console.log(data);
-
-    data.map(({ genres }) => {
-      genres.map((item) => (allGenres[item.id] = item.name));
-    });
-
-    dispatch(getGenres(allGenres));
+    }catch(error){
+      console.log(error);
+    }
   }
 
   return (
     <Router>
+      <VPNDialog isOpen={vpnDialogBoxVisible} handleClose={handleDialogClose} />
       <AppRoutes/>
     </Router>
   );
